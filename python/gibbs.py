e566c82fae827f7abf3ac9afb37a2d3015f2c123
Bryan He <bryanhe@stanford.edu>
2016-07-26 09:07:15 -0700

    sync

--- python/gibbs.py
+++ python/gibbs.py
@@ -5,15 +5,15 @@ from numba import jit, jitclass, autojit, void
 import numpy as np
 import struct
 
-Meta = np.dtype([('weights',   np.int64),
-                 ('variables', np.int64),
-                 ('factors',   np.int64),
-                 ('edges',     np.int64),
-                 ('weights_file', np.str_, 100), # TODO: no max length on string
-                 ('variables_file', np.str_, 100),
-                 ('factors_file', np.str_, 100),
-                 ('edges_file',   np.str_, 100)])
-Meta_ = numba.from_dtype(Meta)
+Meta = np.dtype([('weights',        np.int64),
+                 ('variables',      np.int64),
+                 ('factors',        np.int64),
+                 ('edges',          np.int64),
+                 ('weights_file',   object), # TODO: no max length on string
+                 ('variables_file', object),
+                 ('factors_file',   object),
+                 ('edges_file',     object)])
+#Meta_ = numba.from_dtype(Meta)
 
 # TODO: uint or int
 Weight  = np.dtype([("weightId",     np.int64),
@@ -24,18 +24,25 @@ Weight_ = numba.from_dtype(Weight)
 
 Variable  = np.dtype([("variableId",     np.int64),
                       ("roleSerialized", np.bool),
-                      ("initialValue", np.int32),
-                      ("dataType",     np.int16),
+                      ("initialValue",   np.int32),
+                      ("dataType",       np.int16),
                       ("cardinality",    np.int32)])
 Variable_ = numba.from_dtype(Variable)
 
-Factor  = np.dtype([("variableId",     np.int64),
-                      ("roleSerialized", np.bool),
-                      ("initialValue", np.int32),
-                      ("dataType",     np.int16),
-                      ("cardinality",    np.int32)])
+VariableReference  = np.dtype([("variableId",     np.int64),
+                               ("equalPredicate", np.int32)])
+VariableReference_ = numba.from_dtype(VariableReference)
+
+WeightReference  = np.dtype([("numWeights",     np.int64),
+                             ("equalPredicate", np.int32)])
+WeightReference_ = numba.from_dtype(WeightReference)
+
+Factor  = np.dtype([("factorFunction", np.int16),
+                    ("arity",          np.int32),
+                    ("variable",       VariableReference),
+                    ("weight",         WeightReference)])
 Factor_ = numba.from_dtype(Factor)
-Factor = Variable.newbyteorder(">")
+#Factor = Variable.newbyteorder(">")
 
 
 #spec = [('weight', Weight_),
@@ -72,33 +79,38 @@ def load():
     
     #weighs = np.empty(meta["variables"], Weight)
     
-    weight = np.fromfile("../test/biased_coin/graph.weights", Weight).byteswap()
-    variable = np.fromfile("../test/biased_coin/graph.variables", Variable).byteswap()
+    weight = np.fromfile("../test/biased_coin/graph.weights", Weight).byteswap() # TODO: only if system is little-endian
+    variable = np.fromfile("../test/biased_coin/graph.variables", Variable).byteswap() # TODO: only if system is little-endian
     print(weight)
     print(variable)
-    factor = []
+    factor = np.empty(meta["factors"], Factor)
+    info = []
     with open("../test/biased_coin/graph.factors", "rb") as f: # TODO: should be weights_file...
         try:
             for i in range(meta["factors"]):
                 factor_type = struct.unpack('!h', f.read(2))[0]
-                arity = struct.unpack('!h', f.read(2))[0]
-                f.read(30)
-                variable_id = np.zeros(arity)
-                should_equal_to = np.zeros(arity)
+                arity = struct.unpack('!i', f.read(4))[0]
+                #f.read(28)
+                #variable_id = np.zeros(arity)
+                #should_equal_to = np.zeros(arity)
+                variable_id = np.zeros(0)
+                should_equal_to = np.zeros(0)
                 for j in range(arity):
-                    variable_id[j] = struct.unpack('!q', f.read(8))[0]
-                    should_equal_to[j] = struct.unpack('!q', f.read(8))[0]
+                    #variable_id[j] = struct.unpack('!q', f.read(8))[0]
+                    #should_equal_to[j] = struct.unpack('!i', f.read(4))[0]
+                    struct.unpack('!q', f.read(8))[0]
+                    struct.unpack('!i', f.read(4))[0]
                 #variableId1     long    8
                 #isPositive1     bool    1
                 #variableId2     long    8
                 #isPositive2     bool    1
-                factor += [(factor_type, arity, variable_id.tolist(), should_equal_to.tolist())]
+                info += [(factor_type, arity, variable_id.tolist(), should_equal_to.tolist())]
         except:
             pass
         finally:
             pass
     
-    print(factor)
+    print(info)
     return meta, weight, variable, factor
     #    variable = np.empty(100, Variable)
     #    #variable["value"].value = 1
