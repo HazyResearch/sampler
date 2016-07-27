74938a57a994e93c40b7e7cf8066c0aea24fc7fb
Bryan He <bryanhe@stanford.edu>
2016-07-26 23:26:24 -0700

    prevent dw from multithreading

--- ising/dw_test.sh
+++ ising/dw_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../dw gibbs --burn_in 0 -i 100 -l 0 -m graph.meta -w graph.weights -f graph.factors -v graph.variables
+../dw gibbs --burn_in 0 -i 100 -l 0 -t 1 -c 1 -m graph.meta -w graph.weights -f graph.factors -v graph.variables
