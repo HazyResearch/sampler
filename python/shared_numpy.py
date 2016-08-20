071c96035ab5180e087ffde5d2d687a50fcb71f2
Bryan He <bryanhe@stanford.edu>
2016-08-19 17:44:57 -0700

    example probably is actually working

--- python/shared_numpy.py
+++ python/shared_numpy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import ctypes
 import logging
 import multiprocessing as mp
+import time
+from numba import jit
 
 from contextlib import closing
 
@@ -10,31 +13,46 @@ import numpy as np
 
 info = mp.get_logger().info
 
+ITER = 10000
+
 def main():
-    logger = mp.log_to_stderr()
-    logger.setLevel(logging.INFO)
+    #logger = mp.log_to_stderr()
+    #logger.setLevel(logging.INFO)
 
     # create shared array
-    N, M = 100, 11
+    N, M = 10000, 11
     shared_arr = mp.Array(ctypes.c_double, N)
     arr = tonumpyarray(shared_arr)
 
     # fill with random values
-    arr[:] = np.random.uniform(size=N)
-    arr_orig = arr.copy()
+    arr[:] = np.zeros(N)
+    #arr[:] = np.random.uniform(size=N)
+    #arr_orig = arr.copy()
+
+    start = time.time()
+    h(arr)
+    end = time.time()
+    print("Time:", end - start)
+    print("Range:", min(arr), "-", max(arr))
+    print()
 
     # write to arr from different processes
+    start = time.time()
     with closing(mp.Pool(initializer=init, initargs=(shared_arr,))) as p:
-        # many processes access the same slice
-        stop_f = N // 10
-        p.map_async(f, [slice(stop_f)]*M)
+        ## many processes access the same slice
+        #stop_f = N // 10
+        #p.map_async(f, [slice(stop_f)]*M)
 
         # many processes access different slices of the same array
-        assert M % 2 # odd
-        step = N // 10
-        p.map_async(g, [slice(i, i + step) for i in range(stop_f, N, step)])
+        #assert M % 2 # odd
+        #step = N // 10
+        p.map_async(g, [i for i in range(M)])
     p.join()
-    assert np.allclose(((-1)**M)*tonumpyarray(shared_arr), arr_orig)
+    end = time.time()
+    print("Time:", end - start)
+    print("Range:", min(arr), "-", max(arr))
+    print()
+    #assert np.allclose(((-1)**M)*tonumpyarray(shared_arr), arr_orig)
 
 def init(shared_arr_):
     global shared_arr
@@ -52,8 +70,14 @@ def g(i):
     """no synchronization."""
     info("start %s" % (i,))
     arr = tonumpyarray(shared_arr)
-    arr[i] = -1 * arr[i]
+    h(arr)
     info("end   %s" % (i,))
+
+@jit(nopython=True,cache=True,nogil=True)
+def h(arr):
+    for it in range(ITER):
+        for i in range(len(arr)):
+            arr[i] += 1
 
 if __name__ == '__main__':
     mp.freeze_support()
