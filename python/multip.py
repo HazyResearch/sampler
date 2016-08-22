4c6ee3f7ca635ee121882b1388278f85c10ac855
Bryan He <bryanhe@stanford.edu>
2016-08-22 11:31:08 -0700

    minor multiprocessing tests

--- python/multip.py
+++ python/multip.py
@@ -5,15 +5,21 @@ import numba
 from numba import jit, jitclass, autojit, void
 import numpy as np
 import multiprocessing
+from multiprocessing import sharedctypes
 import ctypes
 import time
 
 N = 10000
-ITER = 10000
+ITER = 1000000
 
-shared_array = multiprocessing.Array('f', 10000, lock=False)
-a = np.zeros(10000)
-shared_array[:10000] = a
+S = np.zeros(N)
+shape = S.shape
+#S.shape = size
+shared_array = sharedctypes.RawArray('d', S)
+S = np.frombuffer(shared_array, dtype=np.float64, count=N)
+S.shape = shape
+#a = np.zeros(10000)
+#shared_array[:10000] = a
 
 class Timer:    
     def __enter__(self):
@@ -24,24 +30,26 @@ class Timer:
         self.end = time.time()
         self.interval = self.end - self.start
 
-#@jit(nopython=True,nogil=True)
-@autojit
-def f(iters,def_param=shared_array):
+def f_wrapper((iters,array)):
+    f(iters, array)
+    print("Range:", min(array), "-", max(array))
+
+@jit(nopython=True,nogil=True)
+def f(iters,array):
     for it in range(iters):
-        for i in range(len(shared_array)):
-            shared_array[i] += 1
+        for i in range(len(array)):
+            array[i] += 1
 
 
-a = np.zeros(10000)
 with Timer() as t:
-    f(ITER)
+    f_wrapper((ITER, S))
 print('Request took %.03f sec.' % t.interval)
 print("Range:", min(shared_array), "-", max(shared_array))
-shared_array[:10000] = a
+#shared_array[:10000] = a
 with Timer() as t:
-    pool = multiprocessing.Pool(processes=10)
-    pool.map(f,[ITER]*10)
+    pool = multiprocessing.Pool(processes=2)
+    pool.map(f_wrapper,[(ITER,S)]*2)
 
 print('Request took %.03f sec.' % t.interval)
-print("Range:", min(shared_array), "-", max(shared_array))
+print("Range:", min(S), "-", max(S))
 
