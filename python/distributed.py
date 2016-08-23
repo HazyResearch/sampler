1b2c00da503fe5817456b1b18ac697722acae1cd
Bryan He <bryanhe@stanford.edu>
2016-08-23 13:57:05 -0700

    clearer messages

--- python/distributed.py
+++ python/distributed.py
@@ -22,7 +22,12 @@ def recv_array(socket, flags=0, copy=True, track=False):
     md = socket.recv_json(flags=flags)
     msg = socket.recv(flags=flags, copy=copy, track=track)
     buf = buffer(msg)
-    A = np.frombuffer(buf, dtype=eval(md['dtype']))
+
+    try:
+        A = np.frombuffer(buf, dtype=md['dtype'])
+    except:
+        A = np.frombuffer(buf, dtype=eval(md['dtype']))
+
     return A.reshape(md['shape'])
 
 def server(argv=None):
@@ -110,15 +115,14 @@ def server(argv=None):
 
     arg = parser.parse_args(argv[1:])
 
-    print(arg.directory)
-
     if arg.directory == "":
         fg = None
     else:
         var_copies = 1
         weight_copies = 1
         (meta, weight, variable, factor, fstart, fmap, vstart, vmap, equalPredicate) = gibbs.load(arg.directory, arg.meta, arg.weight, arg.variable, arg.factor, not arg.quiet, not arg.verbose)
-        fg = gibbs.FactorGraph(weight, variable, factor, fstart, fmap, vstart, vmap, equalPredicate, var_copies, weight_copies)
+        fg_args = (weight, variable, factor, fstart, fmap, vstart, vmap, equalPredicate, var_copies, weight_copies)
+        fg = gibbs.FactorGraph(*fg_args)
 
     context = zmq.Context()
     socket = context.socket(zmq.REP)
@@ -126,27 +130,38 @@ def server(argv=None):
 
     num_clients = 0
 
+    # TODO: barrier between burn, learn, and inference
+    # Probably need to send client id back
     while True:
         #  Wait for next request from client
         message = socket.recv()
-        if len(message) < 1:
-            print("Received message of size 0.", file=sys.stderr)
-            socket.send("?") # Need to reply or this crashes
-        elif message[0] == 'a': # Initial message from client
-            print("Received hello.")
-            socket.send("%d" % num_clients)
+        if message == "HELLO": # Initial message from client
+            print("Received HELLO.")
+            socket.send("CLIENT_ID", zmq.SNDMORE)
+            socket.send_json("%d" % num_clients)
             num_clients += 1
-        elif message[0] == 'b': # Request for factor graph
-            print("Received request for factor graph.")
-            # TODO
+        elif message == 'R_FACTOR_GRAPH': # Request for factor graph
+            client_id = socket.recv_json()
+            print("Received request for factor graph from client #%d." % client_id)
             # TODO: check that fg != None
-            socket.send("%d" % num_clients)
-            num_clients += 1
-        elif message[0] == 'c': # Client ready
+            # TODO
+            socket.send("FACTOR_GRAPH", zmq.SNDMORE)
+            socket.send_json(len(fg_args), zmq.SNDMORE)
+            for a in fg_args:
+                is_array = (type(a) == np.ndarray)
+                socket.send_json(is_array, zmq.SNDMORE)
+                if is_array:
+                    send_array(socket, a, zmq.SNDMORE)
+                else:
+                    socket.send_json(a, zmq.SNDMORE)
+            socket.send("DONE") # TODO: could just not send SNDMORE for last arg
+        elif message == "READY": # Client ready
             print("Received ready.")
-            socket.send("d%d" % arg.burn) # could skip this if arg.burn == 0
-        elif message[0] == 'e' or message[0] == 'g': # Client done with burn/learning
-            if message[0] == 'e': # Done burning
+            # could skip this if arg.burn == 0
+            socket.send("BURN", zmq.SNDMORE)
+            socket.send_json(arg.burn)
+        elif message == 'DONE_BURN' or message == 'DONE_LEARN': # Client done with burn/learning
+            if message == 'DONE_BURN': # Done burning
                 epochs = 0
             else: # Done learning
                 epochs = socket.recv_json()
@@ -154,19 +169,21 @@ def server(argv=None):
                 pass
 
             if epochs < arg.epoch:
-                socket.send("f", zmq.SNDMORE) # Do learning
+                socket.send("LEARN", zmq.SNDMORE)
                 socket.send_json(arg.learn, zmq.SNDMORE)
                 socket.send_json(0.001, zmq.SNDMORE) # TODO
                 send_array(socket, fg.wv)
             else:
-                socket.send("h%d" % arg.inference) # Do inference
-        elif message[0] == 'i': # Client done with inference
+                socket.send("INFERENCE", zmq.SNDMORE)
+                socket.send_json(arg.inference, zmq.SNDMORE)
+                send_array(socket, fg.wv)
+        elif message == 'DONE_INFERENCE': # Client done with inference
             data = recv_array(socket)
             # TODO: handle count
-            socket.send("j")
+            socket.send("EXIT")
         else:
-            print("Message cannot be interpreted.", file=sys.stderr)
-            socket.send("j")
+            print("Message (%s) cannot be interpreted." % message, file=sys.stderr)
+            socket.send("EXIT")
 
     return
 
@@ -245,57 +262,71 @@ def client(argv=None):
     socket.connect ("tcp://localhost:%s" % arg.port)
 
     # hello message
-    socket.send ("a")
+    print("Sent HELLO.")
+    socket.send("HELLO")
     message = socket.recv()
+    assert(message == "CLIENT_ID")
+    message = socket.recv_json()
     client_id = int(message)
-    print("Received id #%d." % client_id)
+    print("Received id #%d.\n" % client_id)
 
     # request factor graph if not loaded
     if fg == None:
-        socket.send("b")
+        socket.send("R_FACTOR_GRAPH", zmq.SNDMORE)
+        socket.send_json(client_id)
+
         message = socket.recv()
-        # TODO: generate factor graph
+        assert(message == "FACTOR_GRAPH")
+
+        length = socket.recv_json()
+        fg_args = [None,] * length
+        for i in range(length):
+            is_array = socket.recv_json()
+            if is_array:
+                fg_args[i] = recv_array(socket)
+            else:
+                fg_args[i] = socket.recv_json()
+        assert(socket.recv() == "DONE")
+
+        fg = gibbs.FactorGraph(*fg_args)
 
     # Send "ready"
-    socket.send("c")
+    socket.send("READY")
 
     learning_epochs = 0
     while True:
         message = socket.recv()
-        if len(message) < 1:
-            print("Received message of size 0.", file=sys.stderr)
-            socket.send("?") # Need to reply or this crashes
-        elif message[0] == 'd': # request for burn-in
+        if message == 'BURN': # request for burn-in
             print("Received request for burn-in.")
-            burn = int(message[1:])
+            burn = socket.recv_json()
             print("Burning", burn, "sweeps.")
             fg.gibbs(burn, 0, 0)
-            socket.send("e")
-        elif message[0] == 'f': # Request for learning
+            socket.send("DONE_BURN")
+        elif message == 'LEARN': # Request for learning
             print("Received request for learning.")
             sweeps = socket.recv_json()
             step = socket.recv_json()
-            print(sweeps)
-            print(step)
+
             fg.wv = recv_array(socket)
             wv = fg.wv
 
             fg.learn(sweeps, step, 0, 0)
 
             dw = fg.wv - wv
-            socket.send("g", zmq.SNDMORE)
+            socket.send("DONE_LEARNING", zmq.SNDMORE)
             learning_epochs += 1
             socket.send_json(learning_epochs, zmq.SNDMORE)
             send_array(socket, dw)
-        elif message[0] == 'h': # Request for inference
+        elif message == 'INFERENCE': # Request for inference
             print("Received request for inference.")
-            inference = int(message[1:])
+            inference = socket.recv_json()
+            fg.wv = recv_array(socket)
             print("Inference:", inference, "sweeps.")
             fg.clear()
             fg.gibbs(inference, 0, 0)
-            socket.send("i", zmq.SNDMORE)
+            socket.send("DONE_INFERENCE", zmq.SNDMORE)
             send_array(socket, fg.count)
-        elif message[0] == 'j': # Exit
+        elif message == 'EXIT': # Exit
             print("Exit")
             break
         else:
